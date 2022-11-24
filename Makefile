7fbe333de9180d6fcb3ce4cf3ac91fb13f052ecd
Maaz <88405217+MaazBinNaseer@users.noreply.github.com>
2022-11-24 14:28:16 +0400

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,18 @@
+NAME =pipex 
+CFILES = childs_cmd.c pipex.c free_and_error.c ft_memcpy.c ft_split.c ft_strdup.c ft_strjoin.c ft_strlen.c ft_strncmp.c 
+OBJECTS = $(CFILES:.c=.o)
+CFLAGS = -Wall -Wextra -Werror 
+CC = gcc
+#generating the compile files 
+$(NAME): $(OBJECTS)
+	$(CC) $(CFLAGS) $(OBJECTS) -o $(NAME) 
+all: $(NAME) 
+# Generating Object files 
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+clean: 
+	rm -rf $(NAME) $(OBJECTS) $(BONUS_OJS)
+fclean: clean 
+	rm -rf $(NAME)
+	
+re: clean all 	
---
+++ pipex.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbin-nas <mbin-nas@student.42.fr>              +#+  +:+       +#+    */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 11:18:10 by mbin-nas            #+#    #+#           */
+/*   Updated: 2022/11/11 11:18:10 by mbin-nas          ###   ########.fr      */
+/*                                                                            */
+/* ************************************************************************** */
+#include "pipex.h"
+
+char *find_path(char **envp)
+{
+    while (ft_strncmp("PATH", *envp, 4))
+        envp++;
+    return (*envp + 5);
+}
+
+int main(int argc, char *argv[], char *envp[])
+{
+    int pointer;
+    t_pipex pipex;
+
+    if(argc != 5)
+        return (message_error(ERR_INPUT));
+    pipex.infile = open(argv[1],O_RDONLY);
+    if(pipex.infile < 0) 
+      message_error(ERR_INFILE);     
+    pipex.outfile = open(argv[argc - 1], O_TRUNC | O_CREAT | O_RDWR, 0000644);
+    if (pipex.outfile < 0)
+         message_error(ERR_OUTFILE);
+    if(pipe(pipex.fd) < 0)
+        message_error(ERR_PIPE);
+    pipex.path = find_path(envp);
+    pipex.cmd_path = ft_split(pipex.path, ':');
+    if(pipex.infile > 0)
+    {
+        pipex.pid1 = fork();
+        if(pipex.pid1 == 0)
+            first_child_process(pipex, argv ,envp);
+    }
+    if(pipex.outfile > 0)
+    {
+        pipex.pid2 = fork();
+    if(pipex.pid2 == 0)
+        second_child_process(pipex, argv, envp);
+    }
+    close_pipes(&pipex);
+    waitpid(pipex.pid1, NULL, 0);
+    waitpid(pipex.pid2, &pointer, 0);
+    free_parent(&pipex);
+    exit(WEXITSTATUS(pointer));
+    return (0);
+}
